54fd585e540cddd9b90572e6e6a0521476155b1a
Zak <z.abdessemed@univ-batna2.dz>
2023-02-26 09:51:39 -0800

    added post_login in authControllers

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,5 +3,5 @@ const router = express.Router()
 
 const authController = require('../controllers/authControllers')
 router.post('/sign-up', authController.post_signUp)
-
+router.post('/login', authController.post_login)
 module.exports = router
