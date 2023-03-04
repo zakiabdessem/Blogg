470b69384b0f7bb084d1e83bbb5dac6108557488
Zak <z.abdessemed@univ-batna2.dz>
2023-03-04 12:50:06 -0800

    removed not needed elements is Header.jsx

--- frontend/src/pages/partials/Header.jsx
+++ frontend/src/pages/partials/Header.jsx
@@ -7,16 +7,6 @@ export default function Header() {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Blogg</title>
-      <link rel="stylesheet" href="/assets/fontawesome/css/all.min.css" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
-        rel="stylesheet"
-      />
-      <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
-      <link href="/assets/css/templatemo-xtra-blog.css" rel="stylesheet" />
-      <script src="/assets/js/templatemo-script.js" />
-      <script src="/assets/js/jquery.min.js" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Mukta:wght@800&display=swap"
