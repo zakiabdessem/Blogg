3d8c98dc059261d4b6c414c5ffa7b6609d1348ff
Zak <z.abdessemed@univ-batna2.dz>
2023-03-10 12:32:42 +0100

    handle image save and upload working -> now working on sending the imageURL to api

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -6,12 +6,9 @@ const router = express.Router();
 
 router.post("/picture/save", authController.verifyToken, (req, res) => {
   const { email, id } = req.decodedToken;
-  const { imageBlob } = req.body;
+  const { imageURL } = req.body;
 
 
-  const buffer = Buffer.from(imageBlob.split(",")[1], "blob");
-
-  console.log("buffer is :", buffer);
 
 });
 
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -3,49 +3,87 @@ import Resizer from "react-image-file-resizer";
 import Cookies from "universal-cookie";
 import axios from "axios";
 import { setToLs, getFromLs } from "../../utils/localstorage";
-import { fill } from "@cloudinary/url-gen/actions/resize";
-import { CloudinaryImage } from "@cloudinary/url-gen";
 
-const cookies = new Cookies();
+const resizeFileBlob = async (
+  file // resize it just for preview
+) =>
+  new Promise((resolve) => {
+    Resizer.imageFileResizer(
+      file,
+      300,
+      300,
+      "JPEG",
+      100,
+      0,
+      (uri) => {
+        resolve(uri);
+      },
+      "blob"
+    );
+  });
 
 export default function Profile() {
   const [error, setError] = useState();
   const [files, setFiles] = useState([]);
+  const [imagePreview, setImagePreview] = useState();
+  const [imageURL, setImageURL] = useState();
 
   const handleImageSave = async (e) => {
-    e.preventDefault();
-    // Sending the image Base64 to API
-    const formData = new FormData();
-    formData.append("file", files[0]);
-    formData.append("upload_preset", "blogg_app");
-    formData.append("cloud_name","dlua23dqn")
+    const file = e.target.files[0];
+    if (file && file.type.substring(0, 5) === "image") {
+      e.preventDefault();
+      // Sending the image Base64 to API
+      const formData = new FormData();
+      formData.append("file", files[0]);
+      formData.append("upload_preset", "blogg_app_ProfilePics");
+      formData.append("cloud_name", "dlua23dqn");
 
-    try {
-      const response = await axios.post(
-        "https://api.cloudinary.com/v1_1/dlua23dqn/image/upload",
-        formData
-      );
-      console.log(response)
-      // Handle the response from the API
-    } catch (error) {
-      console.log(error);
-      // Handle errors
+      try {
+        const response = await axios.post(
+          "https://api.cloudinary.com/v1_1/dlua23dqn/image/upload",
+          formData
+        );
+        if (response.status === 200) setImageURL(response.data.url);
+        // Handle the response from the API
+      } catch (err) {
+        console.log(err);
+        // Handle errors
+      }
+    } else {
+      setError("Invalid image type");
+    }
+  };
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.substring(0, 5) === "image") {
+      const imageBlob = await resizeFileBlob(file);
+
+      setFiles([imageBlob]);
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(imageBlob);
+    } else {
+      setError("Invalid image type");
     }
   };
 
   return (
     <>
       {error && <div>{error}</div>}
-      <form onSubmit={handleImageSave}>
+      <form onSubmit={(e) => handleImageSave(e)}>
         <label htmlFor="image">Upload a profile picture</label>
         <input
           type="file"
           accept="/image/*"
-          onChange={(e) => setFiles(e.target.files)}
+          onChange={(e) => handleImageUpload(e)}
         />
         <button type="submit">Save</button>
+        {imagePreview && <img src={imagePreview} alt="Profile picture"></img>}
       </form>
     </>
   );
 }
-
