6752597b7fbf7b98ed5e8986dedc5327ecf54c92
Zak <z.abdessemed@univ-batna2.dz>
2023-03-10 10:20:48 +0100

    backup -going to create widget -

--- frontend/src/pages/partials/Header.jsx
+++ frontend/src/pages/partials/Header.jsx
@@ -8,7 +8,14 @@ export default function Header() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Blogg</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@800&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Mukta:wght@800&display=swap"
+        rel="stylesheet"
+      />
+      <script
+        src="https://upload-widget.cloudinary.com/global/all.js"
+        type="text/javascript"
+      ></script>
     </Helmet>
   );
 }
--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -3,94 +3,33 @@ import Resizer from "react-image-file-resizer";
 import Cookies from "universal-cookie";
 import axios from "axios";
 import { setToLs, getFromLs } from "../../utils/localstorage";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import { CloudinaryImage } from "@cloudinary/url-gen";
 
 const cookies = new Cookies();
-const resizeFileBlob = async (file) =>
-  new Promise((resolve) => {
-    Resizer.imageFileResizer(
-      file,
-      300,
-      300,
-      "JPEG",
-      100,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      "blob"
-    );
-  });
-const resizeFileBase64 = async (file) =>
-  new Promise((resolve) => {
-    Resizer.imageFileResizer(
-      file,
-      300,
-      300,
-      "JPEG",
-      100,
-      0,
-      (uri) => {
-        resolve(uri);
-      },
-      "base64"
-    );
-  });
-/*useEffect(() => {
-if(!getFromLs("profile_picture")){
-  //TODO: get the image from DB by user id
-}
-
-}, []);*/
 
 export default function Profile() {
   const [error, setError] = useState();
-  const [renderImage, setRenderImage] = useState();
-  const [imageBlob, setImageBlob] = useState();
-  const handleImageUpload = async (e) => {
-    const file = e.target.files[0];
-
-    if (file && file.type.substring(0, 5) === "image") {
-      const _imageBlob = await resizeFileBlob(file);
-      setImageBlob(_imageBlob);
-      const reader = new FileReader();
-      reader.onload = () => {
-        setRenderImage(reader.result);
-      };
-      reader.readAsDataURL(_imageBlob);
-    } else {
-      setError("Invalid image type");
-    }
-  };
+  const [files, setFiles] = useState([]);
 
   const handleImageSave = async (e) => {
     e.preventDefault();
     // Sending the image Base64 to API
-    const data = {
-      imageBlob,
-    };
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    formData.append("upload_preset", "blogg_app");
+    formData.append("cloud_name","dlua23dqn")
 
     try {
-      // Send the image url to your API
       const response = await axios.post(
-        "http://localhost:3000/profile/picture/save",
-        data,
-        {
-          headers: {
-            Authorization: `Bearer ${cookies.get("jwt")}`,
-            "Content-Type": "application/json",
-          },
-          withCredentials: true,
-          maxContentLength: Infinity,
-          maxBodyLength: Infinity,
-        }
+        "https://api.cloudinary.com/v1_1/dlua23dqn/image/upload",
+        formData
       );
-      
-      if (response.status === 200) console.log("status 200", response);
-      // TODO: save base64 to localStorage
-      setToLs("profile_picture", renderImage);
+      console.log(response)
+      // Handle the response from the API
     } catch (error) {
       console.log(error);
-      // TODO: handle error
+      // Handle errors
     }
   };
 
@@ -99,10 +38,14 @@ export default function Profile() {
       {error && <div>{error}</div>}
       <form onSubmit={handleImageSave}>
         <label htmlFor="image">Upload a profile picture</label>
-        <input type="file" accept="/image/*" onChange={handleImageUpload} />
+        <input
+          type="file"
+          accept="/image/*"
+          onChange={(e) => setFiles(e.target.files)}
+        />
         <button type="submit">Save</button>
       </form>
-      {renderImage && <img src={renderImage} alt="Profile picture" />}
     </>
   );
 }
+
